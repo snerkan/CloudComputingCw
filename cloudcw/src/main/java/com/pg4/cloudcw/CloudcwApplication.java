f76b64f52a158896b6f4cfbe02e227019d56db8b
selinerkan <selinerkan@pc>
2018-03-28 13:30:09 +0100

    First Commit

---
+++ cloudcw/src/main/java/com/pg4/cloudcw/CloudcwApplication.java
@@ -0,0 +1,12 @@
+package com.pg4.cloudcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudcwApplication.class, args);
+	}
+}
---
+++ cloudcw/src/test/java/com/pg4/cloudcw/CloudcwApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pg4.cloudcw;
+/*
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudcwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+*/
